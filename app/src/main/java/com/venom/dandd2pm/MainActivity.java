d84c2003472a79043cef96a58b3aaa7850bba3ad
Preston <cpreston@pdx.edu>
2018-03-18 22:38:11 -0700

    g

--- app/src/main/java/com/venom/dandd2pm/MainActivity.java
+++ app/src/main/java/com/venom/dandd2pm/MainActivity.java
@@ -1,22 +1,42 @@
 package com.venom.dandd2pm;
 
 import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.net.wifi.p2p.WifiP2pManager.ChannelListener;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.venom.dandd2pm.wifi.WifiDirectBroadcastReceiver;
+
 public class MainActivity extends AppCompatActivity {
-    WifiP2pManager p2pManager;
-    Looper mainLooper;
-    Context ctx;
-    ChannelListener channelListener;
+    public static final String TAG = "wifidirectdemo";
+    WifiP2pManager mP2PManager;
+    Looper mMainLooper;
+    Context mCtx;
+    ChannelListener mChannelListener;
+    Channel mChannel;
+    WifiDirectBroadcastReceiver mReceiver;
+    IntentFilter mIntentFilter;
+    private boolean isWifiP2pEnabled = false;
+    private boolean retryChannel = false;
+
+    /**
+     * @param isWifiP2pEnabled the isWifiP2pEnabled to set
+     */
+    public void setIsWifiP2pEnabled(boolean isWifiP2pEnabled) {
+        this.isWifiP2pEnabled = isWifiP2pEnabled;
+    }
 
     public Context getCtx() {
-        return ctx;
+        return mCtx;
     }
+    public Looper getMainActivityLooper() { return mMainLooper;}
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -28,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mCtx = this;
+        mMainLooper = getMainLooper();
+
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
@@ -36,7 +59,60 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setUpWifiP2PStuff() {
-        p2pManager.initialize(getCtx(), mainLooper, channelListener);
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        mP2PManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mP2PManager.initialize(mCtx, mMainLooper, mChannelListener);
+        mReceiver = new WifiDirectBroadcastReceiver(mP2PManager, mChannel, this);
+    }
+
+    /* register the broadcast receiver with the intent values to be matched */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
+    }
+    /* unregister the broadcast receiver */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+
+    public void seekPeers() {
+        mP2PManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                //...
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+                //...
+            }
+        });
+    }
+
+    public void connectToPeer(WifiP2pDevice device) {
+        //obtain a peer from the WifiP2pDeviceList
+        WifiP2pConfig config = new WifiP2pConfig();
+        config.deviceAddress = device.deviceAddress;
+        mP2PManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                //success logic
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                //failure logic
+            }
+        });
     }
 
     /**
